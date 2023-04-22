be18540789867305fa9267c1269736b373702819
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-04-22 12:18:56 -0300

    feat: call UserRouter to handle requests in server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import http from "http";
+import { UserRouter } from "./routes/UserRouter";
+
+const PORT = 3001;
+const HOSTNAME = "localhost";
+
+const server = http.createServer((req, res) => {
+	UserRouter.handleRequest(req, res);
+});
+
+server.listen(PORT, HOSTNAME, () => {
+	console.log("Server running");
+});
