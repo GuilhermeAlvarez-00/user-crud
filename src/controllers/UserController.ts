d6f598bf52a06bf354794160443e1bdfc7ffd924
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-04-22 12:28:48 -0300

    feat: add method to create user in controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,8 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { v4 as uuidv4 } from "uuid";
+
 import { UserModel } from "../model/UserModel";
+import { IUser } from "../types";
 
 export class UserController {
 	static getUsers(req: IncomingMessage, res: ServerResponse): void {
@@ -7,5 +10,29 @@ export class UserController {
 
 		res.writeHead(200, { "Content-Type": "application/json" });
 		res.end(JSON.stringify(users));
+	}
+
+	static createUser(req: IncomingMessage, res: ServerResponse): void {
+		let body = "";
+
+		req.on("data", (chunk) => {
+			body += chunk;
+		});
+
+		req.on("end", () => {
+			const { name, email }: Omit<IUser, "id" | "createdAt"> = JSON.parse(body);
+
+			const user: IUser = {
+				id: uuidv4(),
+				name,
+				email,
+				createdAt: new Date(),
+			};
+
+			UserModel.createUser(user);
+
+			res.writeHead(201, { "Content-Type": "application/json" });
+			res.end(JSON.stringify(user));
+		});
 	}
 }
